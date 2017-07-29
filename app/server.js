fa4e3a65d1421ad2b0d0d13565d670305e7144f8
fvarya <f.varya@ya.ru>
2017-07-29 21:05:32 +0300

    auth, rooms

--- app/server.js
+++ app/server.js
@@ -1,10 +1,39 @@
 const express = require('express');
 
-const PORT = 3000;
+const http = require('http');
 const app = express();
+const path = require('path');
+const port = 3000;
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cookieSession = require('express-session');
 
 
-app.listen(PORT, () => {
+app.use(cookieParser());
+
+app.use(bodyParser());
+
+app.use(cookieSession({
+    secret: 'ingipro',
+    key: 'sid',
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        maxAge: null,
+    },
+}));
+
+app.use(express.static(path.join(__dirname)));
+
+app.use(express.static(path.join(__dirname, '..', 'node_modules')));
+
+require('./routes')(app);
+
+const server = http.createServer(app);
+server.listen(port, () => {
     // eslint-disable-next-line
-    console.log(`Server started on localhost:${PORT}`);
+    console.log(`Server started on localhost:${port}`);
 });
+
+const io = require('./socket')(server);
+app.set('io', io);
